49200cfd22999853022aca063a1d0c9ab85e28df
Alexey Trusov <alxtrusov@gmail.com>
2018-11-15 18:00:57 +0400

    make find way algoritm

--- application/modules/navigator/Navigator.py
+++ application/modules/navigator/Navigator.py
@@ -5,6 +5,8 @@ class Navigator:
     vertexes = [] # вершины
     edges = [] # ребра
 
+    ways = [] # просмотренные вершины (для поиска пути)
+
     def __init__(self, db, mediator, mapId):
         self.db = db
         self.mediator = mediator
@@ -21,40 +23,44 @@ class Navigator:
         result = [x for x in self.vertexes if x['id'] == id]
         return result[0] if len(result) else None
 
-    # алгоритм поиска путей
-    def findWays(self, startId, finishId, ways):
-        return True
-        #result = [x for x in self.edges if x['vertex1'] == startId or x['vertex2'] == startId]
-        #if len(result):
-        #    for edge in result:
-        #        if edge['vertex1'] == finishId or edge['vertex2'] == finishId: # нашли конечную точку маршрута
-        #            return ways.append(edge['id'])
-        #        else: 
-        #            ways.append(edge['id'])
-        #return self.findWays(, finishId, ways)
-
-
     # найти путь полета
-    def findWay(self, start, finish):
+    def findWays(self, start, finish):
+        
+        # пометить вершину пройденной
+        print(start)
+        
+        edges = [x for x in self.edges if x['vertex1'] == start['id'] or x['vertex2'] == start['id']]
+        for edge in edges:
+            if edge['vertex1'] == finish['id'] or edge['vertex2'] == finish['id']: # если выход - ништяк
+                for way in self.ways: # дописать выход в выходной путь
+                    if (way[len(way) - 1]) == start['id']:
+                        way.append(finish['id'])
+                return True
+            # взять все выходы из вершины, кроме уже пройденных
+            for way in self.ways: # найти непомеченную вершину
+                if (not len([x for x in way if x == edge['vertex1']])):
+                    way.append(edge['vertex1'])
+                    self.findWays([x for x in self.vertexes if x['id'] == edge['vertex1']][0], finish)
+                if (not len([x for x in way if x == edge['vertex2']])):
+                    way.append(edge['vertex2'])
+                    self.findWays([x for x in self.vertexes if x['id'] == edge['vertex2']][0], finish)
+            #...
 
-        return [x for x in self.edges if x['vertex1'] == start['id'] or x['vertex2'] == start['id']]
-
-        #ways = []
-        #self.findWays(start['id'], finish['id'], ways)
-        #return ways
+        # иначе повторить для всех остальных выходов
+        # если выходов больше одного - продублировать записи с путями
 
     # сделать полетное задание
     def makeOrder(self, options):
-        start  = self.getVertex(options['start' ])
-        finish = self.getVertex(options['finish'])
+        start  = self.getVertex(options['start' ]) # точка старта маршрута
+        finish = self.getVertex(options['finish']) # точка финиша маршрута
         if start and finish: 
-            way = self.findWay(start, finish) # путь
-
-            print(way)
-
-            #... полет
-            #... разгрузка
-            #backWay = self.findWay(finish, start, self.edges) # путь обратно
-            #... полет обратно
-            return True
+            self.ways = [] # обнулить список путей
+            self.ways.append([start['id']])
+            if self.findWays(start, finish): # поиск путей
+                print('Путь нашелся!!!', self.ways)
+                #... полет
+                #... разгрузка
+                #backWay = self.findWay(finish, start, self.edges) # путь обратно
+                #... полет обратно
+                return True
         return False
